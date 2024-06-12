78b5cb906cd876f93338ae149f0d862572ddc087
Manuel1Aguilar <manuelmla.aguilar@gmail.com>
2024-06-12 17:38:33 -0300

    Rotate image solution

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace LeetCodeSolutions
     {
         static void Main(string[] args)
         {
-            ValidSudoku.CallSolution();
+            RotateImage.CallSolution();
         }
         public static void SumOfRevLinkedListNumsSolutions()
         {
--- TopInterviewQuestionsCollection/Array/RotateImage.cs
+++ TopInterviewQuestionsCollection/Array/RotateImage.cs
@@ -4,7 +4,29 @@ namespace LeetCodeSolutions.TopInterviewQuestionsCollection.Array
     {
         private static void RotateImageInPlace(int[][] matrix)
         {
-            
+            for (int i = matrix.Length - 1; i > 0; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    (matrix[i][j], matrix[j][i]) = (matrix[j][i], matrix[i][j]);
+                }
+            }
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                RotateArray(matrix[i]);
+            }
+        }
+
+        private static void RotateArray(int[] array)
+        {
+            int start = 0;
+            int end = array.Length - 1;
+            while (start < end)
+            {
+                (array[start], array[end]) = (array[end], array[start]);
+                start++;
+                end--;
+            }
         }
 
         public static void CallSolution()
@@ -26,7 +48,7 @@ namespace LeetCodeSolutions.TopInterviewQuestionsCollection.Array
                 Console.WriteLine(String.Join(", ", row));
             }
 
-            input = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]];
+            input = [[5, 1, 9, 11], [2, 4, 8, 10], [13, 3, 6, 7], [15, 14, 12, 16]];
             Console.WriteLine("Input:");
             foreach (int[] row in input)
             {
